d5f8856df426f7baf52e4cf3371c6cbfba0c25e0
Aleksandr Chertkov <alexiiilaiho@gmail.com>
2026-07-28 23:06:58 +0300

    fix: update publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -84,7 +84,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js',
-    publicPath: 'https://alex-devstar.github.io/stellar-burgers/'
+    publicPath: './'
   },
   devServer: {
     static: path.join(__dirname, './dist'),
